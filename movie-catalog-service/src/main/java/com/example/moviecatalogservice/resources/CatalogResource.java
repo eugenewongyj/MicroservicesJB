3253e96223cab42b21fe4e4dfbf3f86c482e026f
eugenewongyj <eugenewongyj@gmail.com>
2022-03-28 13:33:44 +0800

    07. Used RestTemplate to call Movie Info API

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/CatalogResource.java
@@ -1,20 +1,47 @@
 package com.example.moviecatalogservice.resources;
 
 import com.example.moviecatalogservice.models.CatalogItem;
+import com.example.moviecatalogservice.models.Movie;
+import com.example.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/catalog")
 public class CatalogResource {
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        return Collections.singletonList(new CatalogItem("Test", "Test Desc", 4));
+        // Need to first create an instance of RestTemplate
+        // RestTemplate is the utility object that lets us make these calls
+        // RestTemplate takes in the json payload and makes it into the class object
+        //RestTemplate restTemplate = new RestTemplate();
+        //Movie movie = restTemplate.getForObject("http://localhost:8081/movies/foo", Movie.class);
+
+        // Hard coded to simulate data from Rating api. Because the Rating api is currently not coded completely
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("5678", 3)
+        );
+
+        // For each rating, should call Movie api but not now.
+        return ratings.stream().map(rating -> {
+            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+            return new CatalogItem(movie.getName(), "Description", rating.getRating());
+        })
+       .collect(Collectors.toList());
+
     }
 }
