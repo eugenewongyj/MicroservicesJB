d3731977c053e4fd9af4e5e29a43963781696a20
eugenewongyj <eugenewongyj@gmail.com>
2022-04-17 22:09:50 +0800

    07a. Used WebClient to call Movie Info API

--- movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/CatalogResource.java
+++ movie-catalog-service/src/main/java/com/example/moviecatalogservice/resources/CatalogResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,8 +23,14 @@ public class CatalogResource {
     @Autowired
     private RestTemplate restTemplate;
 
+    @Autowired
+    private WebClient.Builder webClientBuilder;
+
     @GetMapping("/{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
+
+
+
         // Need to first create an instance of RestTemplate
         // RestTemplate is the utility object that lets us make these calls
         // RestTemplate takes in the json payload and makes it into the class object
@@ -37,11 +44,23 @@ public class CatalogResource {
         );
 
         // For each rating, should call Movie api but not now.
+//        return ratings.stream().map(rating -> {
+//            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+//            return new CatalogItem(movie.getName(), "Description", rating.getRating());
+//        })
+//       .collect(Collectors.toList());
+
         return ratings.stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
-            return new CatalogItem(movie.getName(), "Description", rating.getRating());
-        })
-       .collect(Collectors.toList());
+                    Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class)
+                            .block();
+
+                    return new CatalogItem(movie.getName(), "Description", rating.getRating());
+                })
+                .collect(Collectors.toList());
 
     }
 }
